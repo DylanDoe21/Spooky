50fb7b20f5b9d5f923e11d379af61956566de4a4
Mortis3 <53282720+DylanDoe21@users.noreply.github.com>
2023-06-18 18:29:41 -0400

    renamed big bone shield texture

--- Content/Backgrounds/SpookyBiome/SpookyUndergroundBG.cs
+++ Content/Backgrounds/SpookyBiome/SpookyUndergroundBG.cs
@@ -19,7 +19,7 @@ namespace Spooky.Content.Backgrounds.SpookyBiome
         {
 			for (int i = 0; i <= 3; i++)
 			{
-                textureSlots[i] = BackgroundTextureLoader.GetBackgroundSlot("Spooky/Content/Backgrounds/SpookyBiome/SpookyUG" + i.ToString());
+                textureSlots[i] = BackgroundTextureLoader.GetBackgroundSlot("Spooky/Content/Backgrounds/SpookyBiome/SpookyUGAlt" + i.ToString());
 			}
 		}
 	}
--- Content/NPCs/Boss/BigBone/BigBone.cs
+++ Content/NPCs/Boss/BigBone/BigBone.cs
@@ -355,7 +355,7 @@ namespace Spooky.Content.NPCs.Boss.BigBone
 
                 var center = NPC.Center - Main.screenPosition + new Vector2(0, NPC.gfxOffY);
                 float intensity = fade;
-                DrawData drawData = new DrawData(ModContent.Request<Texture2D>("Spooky/ShaderAssets/SkullEffect").Value, center - new Vector2(0, -55), 
+                DrawData drawData = new DrawData(ModContent.Request<Texture2D>("Spooky/ShaderAssets/BigBoneShield").Value, center - new Vector2(0, -55), 
                 new Rectangle(0, 0, 500, 400), Color.Orange, 0, new Vector2(250f, 250f), NPC.scale * (1f + intensity * 0.05f), SpriteEffects.None, 0);
                 GameShaders.Misc["ForceField"].UseColor(new Vector3(1f + intensity * 0.5f));
                 GameShaders.Misc["ForceField"].Apply(drawData);
